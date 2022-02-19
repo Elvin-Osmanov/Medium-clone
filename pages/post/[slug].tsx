52069be979eb451a557fe289e191aa653c9b6037
elvin-osmanov <elvinfo@code.edu.az>
2022-02-19 14:56:33 +0400

    style fix

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -50,7 +50,7 @@ const Post = ({ post }: Props) => {
         alt=""
       />
 
-      <article className="ml-auto max-w-3xl p-5">
+      <article className="mx-auto max-w-3xl p-5">
         <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
         <h2 className="mb-2 text-xl font-light text-gray-500">
           {post.description}
